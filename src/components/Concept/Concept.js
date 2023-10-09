807a222a83858a619bec54283cfb8055807360e5
zinminoo2004 <zinminoo.personal@gmail.com>
2023-10-09 17:29:18 +0630

    first commit

---
+++ src/components/Concept/Concept.js
@@ -0,0 +1,11 @@
+const Concepts = (props) => {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+};
+
+export default Concepts;
